aaad9a5fd8549c1bb56d29fddd83e038e5b555e1
Jay <53712717+EungyuCho@users.noreply.github.com>
2022-02-04 13:49:15 +0900

    fix: 컬러 생성 및 정답 색상 변경 기능의 버그를 해결한다

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import Styled from './index.style'
-import { changeColor } from 'src/utils/color'
+import React, { useMemo } from "react";
+import Styled from "./index.style";
+import { changeColor } from "src/utils/color";
 
-const Card = ({ isAnswer, color, size, onCardClick }: CardProps) => {
-  const backgroundColor = '#' + (isAnswer ? changeColor(color) : color)
+const Card = ({ isAnswer, color, size, stage, onCardClick }: CardProps) => {
+  const backgroundColor = useMemo(
+    () =>
+      "#" + (isAnswer ? changeColor(color, Math.max(1, 50 - stage)) : color),
+    [color]
+  );
+
+  if (isAnswer) {
+    console.log(color, backgroundColor);
+  }
 
   return (
     <Styled.Card
-      style={{ backgroundColor: backgroundColor, width: size, height: size }}
+      style={{ backgroundColor, width: size, height: size }}
       onClick={onCardClick(isAnswer)}
     />
-  )
-}
+  );
+};
 
 interface CardProps {
-  isAnswer: boolean
-  color: string
-  size: number
-  onCardClick: (isAnswer: boolean) => () => void
+  isAnswer: boolean;
+  color: string;
+  stage: number;
+  size: number;
+  onCardClick: (isAnswer: boolean) => () => void;
 }
 
-export default React.memo(Card)
+export default React.memo(Card);
--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,14 +1,39 @@
 function generateRandomHexColor() {
-  return Math.floor(Math.random() * 16777215).toString(16)
+  return Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0");
 }
 
-function changeColor(col: string, amt = 20) {
-  const hexNumber = parseInt(col, 16)
-  return (
-    ((hexNumber & 0x0000ff) + amt) |
-    ((((hexNumber >> 8) & 0x00ff) + amt) << 8) |
-    (((hexNumber >> 16) + amt) << 16)
-  ).toString(16)
+function changeColor(col: string, amt = 50) {
+  const colArray = col.split("");
+  const colors = colArray
+    .reduce(
+      (next, cur) => {
+        if (next[next.length - 1].length === 2) {
+          return [...next, [cur]];
+        }
+
+        next[next.length - 1].push(cur);
+        return next;
+      },
+      [[]]
+    )
+    .map((color) => color.join(""))
+    .map((color) => parseInt(color, 16));
+
+  const colorChangeIndex = Math.floor(Math.random() * 3);
+
+  const changeTarget = colors[colorChangeIndex];
+
+  if (changeTarget + amt > 255) {
+    colors[colorChangeIndex] -= amt;
+  } else {
+    colors[colorChangeIndex] += amt;
+  }
+
+  console.log("colors", colors);
+
+  return colors.map((color) => color.toString(16).padStart(2, "0")).join("");
 }
 
-export { changeColor, generateRandomHexColor }
+export { changeColor, generateRandomHexColor };
